da858bfd6721b1d291b46b7b6ef889c9b389f5c7
DriverStation <jackchampagne.r@gmail.com>
2019-01-21 16:21:34 -0500

    Update JoystickMap.java

---
+++ src/main/java/frc/robot/util/JoystickMap.java
@@ -0,0 +1,15 @@
+package frc.robot.util;
+
+public class JoystickMap {
+    public static final int THROTTLE_AXIS_ID = 1;
+    public static final int STRAFE_AXIS_ID = 0;
+    public static final int TURN_AXIS_ID = 0;
+    
+    public static final int LIFTMODE_TOGGLE_ID = 2;
+    public static final int DRIVEMODE_TOGGLE_ID = 4;
+    public static final int HATCHHOLDER_TOGGLE_ID = 8;
+    public static final int CARGOINTAKE_TOGGLE_ID = 1;
+    public static final int CARGOHOLDER_TOGGLE_ID = 3;
+    public static final int LIFTLEVELINCREASE = 5;
+    public static final int LIFTLEVELDECREASE = 6;
+}
